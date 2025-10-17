a940caa304742b5014ee11e7085f7d6c6679a58f
Nguyễn Minh Nhựt <nhut.nguyen.dev@outlook.com.vn>
2025-10-17 15:10:24 +0700

    feat(login): add auth middleware

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
+from datetime import datetime, timezone
+
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, redirect, request, url_for
+from flask_dance.contrib.google import google
 
 import app.views as bp
 from app.extensions import db
@@ -13,5 +16,19 @@ def create_app():
 
     db.init_app(app)
     bp.init_app(app)
+
+    @app.before_request
+    def check_token():
+        exclude_routes = ['landing.index', 'google.login', 'google.authorized']
+        if request.endpoint in exclude_routes:
+            return
+
+        if not google.authorized:
+            return redirect(url_for('landing.index'))
+
+        exp = google.token['expired_at']
+        now = datetime.now(timezone.utc).timestamp()
+        if now >= exp:
+            return redirect(url_for('landing.index'))
 
     return app
