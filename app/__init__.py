dce3d64a52b036792f085d4ae058ba62ea5079ac
Nguyễn Minh Nhựt <nhut.nguyen.dev@outlook.com.vn>
2025-10-17 15:06:44 +0700

    feat(login): implement login

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,17 @@
-from app.extensions import db
+from dotenv import load_dotenv
 from flask import Flask
+
+import app.views as bp
+from app.extensions import db
 
 
 def create_app():
+    load_dotenv()
+
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_pyfile('config.py')
 
     db.init_app(app)
+    bp.init_app(app)
 
     return app
