a998922c178ecc22a28ab1deeb53e0a9b6b91890
Nguyễn Minh Nhựt <nhut.nguyen.dev@outlook.com.vn>
2025-10-18 18:57:42 +0700

    refactor(auth): wrap route checks with decorator

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,5 @@
-from datetime import datetime, timezone
-
 from dotenv import load_dotenv
-from flask import Flask, redirect, request, url_for
-from flask_dance.contrib.google import google
+from flask import Flask
 
 import app.views as bp
 from app.extensions import db
@@ -16,19 +13,5 @@ def create_app():
 
     db.init_app(app)
     bp.init_app(app)
-
-    @app.before_request
-    def check_token():
-        exclude_routes = ['landing.index', 'google.login', 'google.authorized']
-        if request.endpoint in exclude_routes:
-            return
-
-        if not google.authorized:
-            return redirect(url_for('landing.index'))
-
-        exp = google.token['expires_at']
-        now = datetime.now(timezone.utc).timestamp()
-        if now >= exp:
-            return redirect(url_for('landing.index'))
 
     return app
---
+++ app/utils/decorators.py
@@ -0,0 +1,34 @@
+from datetime import datetime, timezone
+from functools import wraps
+
+from flask import redirect, session, url_for
+from flask_dance.contrib.google import google
+
+
+def token_required(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        if not google.authorized:
+            return redirect(url_for('landing.index'))
+
+        exp = google.token['expires_at']
+        now = datetime.now(timezone.utc).timestamp()
+        if now >= exp:
+            session.pop('google_oauth_token', None)
+            return redirect(url_for('landing.index'))
+
+        return f(*args, **kwargs)
+    return wrapper
+
+
+def anonymous_required(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        if google.authorized:
+            exp = google.token['expires_at']
+            now = datetime.now(timezone.utc).timestamp()
+            if now < exp:
+                return redirect(url_for('user.me'))
+
+        return f(*args, **kwargs)
+    return wrapper
